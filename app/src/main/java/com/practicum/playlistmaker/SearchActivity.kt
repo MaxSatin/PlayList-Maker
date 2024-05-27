d99f3e7de18cbdb28d271a80203f6da05e644963
Maxim <satinmaksim@yandex.ru>
2024-05-27 18:21:15 +0300

    Created inputManager for keyboard

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,10 +1,12 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -17,12 +19,14 @@ class SearchActivity : AppCompatActivity() {
         val buttonBack = findViewById<Button>(R.id.buttonSearchBack)
         val inputEditText = findViewById<EditText>(R.id.editTextwather)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
 
         buttonBack.setOnClickListener {
             finish()
         }
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
 
         val editTextWatcher = object : TextWatcher {
