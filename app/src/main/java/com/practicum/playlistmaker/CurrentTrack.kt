4562f8631b54be66a12f931f327668fdd00c9a6d
Maxim <satinmaksim@yandex.ru>
2024-08-21 23:45:42 +0300

    ЬуÐMedia Player Added

--- app/src/main/java/com/practicum/playlistmaker/CurrentTrack.kt
+++ app/src/main/java/com/practicum/playlistmaker/CurrentTrack.kt
@@ -10,7 +10,8 @@ data class CurrentTrack(
     val collectionName: String,
     val releaseDate: String,
     val primaryGenreName: String,
-    val country: String
+    val country: String,
+    val previewUrl: String
 ) {
     fun getCoverArtWork(): String{
         return artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
--- app/src/main/java/com/practicum/playlistmaker/MediaPlayerComponent.kt
+++ app/src/main/java/com/practicum/playlistmaker/MediaPlayerComponent.kt
@@ -1,4 +1,47 @@
 package com.practicum.playlistmaker
 
+import android.media.MediaPlayer
+import android.widget.Button
+import android.widget.ToggleButton
+
 class MediaPlayerComponent {
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSE = 3
+    }
+
+    private var mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+    lateinit var playButton: Button
+
+
+    private fun preparePlayer(url: String, button: Button) {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton = button
+            button.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playerState = STATE_PAUSE
+    }
+
+    private fun playBackControl() {
+        when(playerState) {
+            STATE_PLAYING -> pausePlayer()
+            STATE_PREPARED, STATE_PAUSE -> startPlayer()
+        }
+
+    }
 }
