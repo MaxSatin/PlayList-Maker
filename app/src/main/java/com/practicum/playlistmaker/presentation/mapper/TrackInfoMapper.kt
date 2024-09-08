f7d52d2032054d93e54533e2b5d2068217b5495e
Maxim <satinmaksim@yandex.ru>
2024-09-08 13:14:12 +0300

    Commit before SearchActivity refactor

--- app/src/main/java/com/practicum/playlistmaker/presentation/mapper/TrackInfoMapper.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/mapper/TrackInfoMapper.kt
@@ -1,4 +1,23 @@
 package com.practicum.playlistmaker.presentation.mapper
 
+import com.practicum.playlistmaker.data.dto.TrackDto
+import com.practicum.playlistmaker.domain.model.Track
+import com.practicum.playlistmaker.presentation.model.TrackInfo
+
 class TrackInfoMapper {
+    fun map (track: Track): TrackInfo {
+        return TrackInfo(
+            trackId = track.trackId,
+            trackName = track.trackName,
+            artistName = track.artistName,
+            trackTimeMillis = track.trackTimeMillis,
+            previewUrl = track.previewUrl,
+            artworkUrl60 = track.artworkUrl60,
+            artworkUrl100 = track.artworkUrl100,
+            collectionName = track.collectionName,
+            releaseDate = track.releaseDate,
+            primaryGenreName = track.primaryGenreName,
+            country = track.country
+        )
+    }
 }
